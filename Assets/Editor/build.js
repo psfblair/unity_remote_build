a7f49edfaba2f438cddb0dc8f01ff2565f913a0d
Paul Blair <pblair@example.com>
2017-04-03 01:34:01 -0400

    Fixes to get remote Unity build working correctly with spaces in pathnames, and with proper command line argument parsing in Unity build.

--- Assets/Editor/build.js
+++ Assets/Editor/build.js
@@ -1,15 +1,29 @@
 import System.Diagnostics;
+import System.Collections.Generic;
 
 class Autobuild {
 
     static function Build() {
         // Get filename.
         var path = "App";
+
+        // These are all the command line arguments to Unity.exe
         var commandLineArgs = System.Environment.GetCommandLineArgs();
-        // First command line argument is the name of this Function
-        var scenes : String[] = commandLineArgs.splice(0, 1);
+
+        // We want everything after "-executeMethod Autobuild.Build"
+        var scenes = new List.<String>();
+        var isAfterAutobuild = false;
+        
+        for (i=1; i < commandLineArgs.length; i++) {
+            if (isAfterAutobuild) {
+                scenes.Add(commandLineArgs[i]);
+            }
+            if (commandLineArgs[i] == "Autobuild.Build") {
+                isAfterAutobuild = true;
+            }
+        }
         
         // Build player.
-        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.WSAPlayer, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes.ToArray(), path, BuildTarget.WSAPlayer, BuildOptions.None);
     }
 }
--- unity_headless_build.bat
+++ unity_headless_build.bat
@@ -4,13 +4,24 @@ REM Remaining command line arguments represent scenes to build, e.g.,
 REM        Assets/Intro.unity Assets/ConwayLife.unity 
 
 SET OUTPUT_DIR=App
+SET MY_DIR=%~dp0
 
-cd %~dp0
+cd %MY_DIR%
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
 REM First comnand line argument
-SET UNITY_FOLDER=%~f1
-REM Get all but first command line argument 
-for /f "tokens=1,* delims= " %%a in ("%*") do set SCENES_TO_BUILD=%%b
+SET UNITY_EXECUTABLE=%~fp1\Editor\Unity.exe
+REM Strip quotes
+SET UNITY_EXECUTABLE=%UNITY_EXECUTABLE:""=%
 
-"%UNITY_FOLDER%" -batchmode -quit -projectPath %~dp0 -executeMethod Autobuild.Build %SCENES_TO_BUILD%
+REM Get all but first command line argument 
+set SCENES_TO_BUILD=
+shift
+:loop1
+if "%1"=="" goto after_loop
+set SCENES_TO_BUILD=%SCENES_TO_BUILD% %1
+shift
+goto loop1
+:after_loop
+
+"%UNITY_EXECUTABLE%" -batchmode -quit -projectPath %MY_DIR% -logFile "%MY_DIR%build.log" -executeMethod Autobuild.Build %SCENES_TO_BUILD%
